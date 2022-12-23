b9de62de59441074cccb3b0f31176bd2f5c63889
mrechavez <milesechavez@gmail.com>
2022-12-23 16:00:10 -0500

    added medium screen media query for nav and JvaScript for nav

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
 })
 
+
 // CONTACT BUTTONS (circular text buttons)
 const textButtons = document.querySelectorAll('.contact__btn');
 
@@ -11,6 +12,7 @@ textButtons.forEach(textButton => {
 
     text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
 })
+
 
 // SWIPER JS (gallery section)
 var swiper = new Swiper(".mySwiper", {
@@ -31,3 +33,33 @@ var swiper = new Swiper(".mySwiper", {
         }
     }
   });
+
+
+  const nav = document.querySelector('.nav__links');
+  const openNavBtn = document.querySelector('#nav__toggle-open');
+  const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+  const openNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+  }
+
+  openNavBtn.addEventListener('click', openNav);
+
+  const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+  }
+
+  closeNavBtn.addEventListener('click', closeNav);
+
+
+
+  // close nav menu on click of menu link
+  if(document.body.clientWidth < 1024) {
+    nav.querySelectorAll('li a').forEach(navLink => {
+      navLink.addEventListener('click', closeNav);
+    })
+  }
